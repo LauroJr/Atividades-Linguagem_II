3a72897c9eb92e6b1a984a7c0deea8212f141e70
Lauro Jr <lauro.juniorspx@gmail.com>
2020-03-09 20:53:57 -0700

    Versão_01

---
+++ Soma_Dois_Num.py
@@ -0,0 +1,8 @@
+from occupation import calcula_soma
+# este código esta usando uma função pronta p/ somar dois números
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+
+Total = calcula_soma(n1, n2)
+print(Total)
