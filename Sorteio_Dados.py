41fa377c7db17bf7cf751d9f3009c2132be31833
Lauro Jr <lauro.juniorspx@gmail.com>
2020-03-09 20:57:03 -0700

    Versão_01

---
+++ Sorteio_Dados.py
@@ -0,0 +1,17 @@
+from random import randint
+
+
+lista1 = []
+for I in range(1,11):
+    lista1.append(randint(1, 6))
+
+print(lista1) 
+
+for I in range(1, 7):
+    n = lista1.count(I)
+    if n > 1:
+        print("O número ",I," aparece ",n," vezes.")
+    if n == 1:
+        print("O número ",I," aparece ",n," vez.")    
+
+
