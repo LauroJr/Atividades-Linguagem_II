20623131d2e6ee32624aec14876802289429b92d
Lauro Jr <lauro.juniorspx@gmail.com>
2020-05-12 17:07:58 -0700

    Exemplos incluídos

--- POO/Polimorfismo/Exemplo_Classe_abstrata_II.py
+++ POO/Polimorfismo/Exemplo_Classe_abstrata_II.py
@@ -11,7 +11,7 @@ class Pessoa(ABC):
     salario = 0
 
     @abstractmethod
-    def calcular_salario(self):
+    def calcular_salario(self, total):
         pass
 
 
@@ -19,10 +19,20 @@ class Funcionario(Pessoa):
     def __init__(self, nome):
         self.nome = nome
 
-    def calcular_salario(self):
-        return self.salario * 1.2
+    def calcular_salario(self, total):
+        return self.salario * 1.2 + total
+
+
+class Medico(Funcionario):
+    def calcular_salario(self, total):
+        return self.salario * 10
 
 
 func = Funcionario('Lauro Mendes do Amaral Junior')
 func.salario = 1000
-print(f'Nome: {func.nome}\nSalário Atualizado: {func.calcular_salario()}')
+print(f'Nome: {func.nome}\nSalário Atualizado: {func.calcular_salario(2568)}')
+print()
+print('TESTE: ')
+medico = Medico('Dr Pamplona')
+medico.salario = 50
+print(f'Nome: {medico.nome}\nSalário Atualizado: {medico.calcular_salario(0)}')
