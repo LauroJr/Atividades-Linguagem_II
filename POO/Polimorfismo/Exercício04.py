3536be2b66a64d800ab43be7890fc58424225f4f
Lauro Jr <lauro.juniorspx@gmail.com>
2020-05-12 17:09:45 -0700

    Versão 1 ok

--- POO/Polimorfismo/Exercício04.py
+++ POO/Polimorfismo/Exercício04.py
@@ -16,3 +16,53 @@ Implemente um programa principal que cria um objeto de cada tipo (gerente, assis
 e os armazena em uma lista
 Percorra essa lista e imprima o salário de cada funcionário.
 '''
+
+from abc import ABC, abstractmethod
+
+
+class Funcionario(ABC):
+    nome = None
+    matricula = None
+    salario_base = 1300.00
+
+    @abstractmethod
+    def calcular_salario(self, salario):
+        pass
+
+
+class Gerente(Funcionario):
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+    def calcular_salario(self):
+        return self.salario_base * 2
+
+
+class Assistente(Funcionario):
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+    def calcular_salario(self):
+        return self.salario_base
+
+
+class Vendedor(Funcionario):
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+    def calcular_salario(self):
+        return self.salario_base * 10 / 100 + self.salario_base
+
+
+gerente = Gerente('Lauro Jr', '154825')
+assistente = Assistente('Mônica Albuquerque', '132649')
+vendedor = Vendedor('Alisson Moaraes', '132648')
+
+lista_func = [gerente, assistente, vendedor]
+
+for func in lista_func:
+    print(f'Nome: {func.nome}\nSalário: {func.calcular_salario()}')
+    print()
