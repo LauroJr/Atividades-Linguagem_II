9ebd1b24465a7bc3fd7679d0af308ec32b7a5666
Lauro Jr <lauro.juniorspx@gmail.com>
2020-05-12 17:14:08 -0700

    PEP8 revisado

--- POO/Polimorfismo/Exercício05.py
+++ POO/Polimorfismo/Exercício05.py
@@ -13,9 +13,12 @@ Já para a conta com limite é necessário guardar o valor do limite.
 As operações possíveis são: depósito, saque e impressão de saldo.
 Essas operações devem ser definidas numa classe abstrata denominada Conta.
 A operação de depósito e saldo são iguais para os três tipos de conta.
-A operação de saque só é diferente na conta com l imite, pois esta admite que o saldo fique
+A operação de saque só é diferente na conta com limite, pois esta admite que o saldo fique
 negativo até o limite estabelecido.
 Nas demais contas o saque não pode ser realizado se não houver saldo
 suficiente.
 Implemente a hierarquia de classes definida acima.
 '''
+
+
+
